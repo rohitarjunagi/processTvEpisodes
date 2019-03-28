3ccaee6b62115578fae115c4410de447888d60d6
Rohit Arjunagi <rohitarjunagi@gmail.com>
2019-03-28 13:42:12 +1100

    Refactor the express server and change dependencies in package.json files

--- app.js
+++ app.js
@@ -1,64 +1,47 @@
-//import required modules
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var processEpisodes = require('./ProcessEpisodes');
-//grab an instance of express
-var app = express();
-//instantiate the processEpisodes Library
-var processEpisodesData = new processEpisodes();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-//configure express
-app.set('port', (process.env.PORT || 5000));
+const indexRouter = require('./routes/index');
+const processTvEpisodesRouter = require('./routes/processTvEpisodes');
+
+const app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(processEpisodesData.setupRoutes());
 
-app.get('/', function(request, response) {
-  response.status(200).send('Please post to this url along with the post data in json format');
-});
-
+app.use('/', indexRouter);
+app.use('/', processTvEpisodesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-// error handlers
-app.use(function(err, req, res, next) {
-  res.header("Content-Type", "application/json");
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+
   if (err.status === 400) {
     res.status(400).send({
       "error": "Could not decode request: JSON parsing failed"
     });
-  } else {
-    res.status(err.status).send(err.message);
   }
-});
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//Start the server on a the specified port
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-//print stack trace for uncaught exceptions
-process.on('uncaughtException', function(err) {
-  console.error('#--------- uncaughtException --------#');
-  console.error(err);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
